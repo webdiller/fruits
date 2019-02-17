93db231ee3dc3035feed5a5073a9859545870de2
Eugene <eugenefromrus@gmail.com>
2019-02-17 11:13:10 +1000

    feature1 commit1

--- js/common.js
+++ js/common.js
@@ -1,18 +1,23 @@
-$(document).ready(function () {
-    // menu toggle BEGIN
+window.addEventListener("load", function(event) {
+  $(".menu").on("click", function () { 
+    $(this).toggleClass("active");
+    $(".overlay").toggleClass("menu-open");
+ });
+$(".nav a").on("click", function () { 
+   $(".menu").toggleClass("active");
+   $(".overlay").toggleClass("menu-open");
+ });
+ var preloader = document.querySelector(".preloader");
+ var mainTextH1 = document.querySelector(".main-text h1");
+ var mainTextH2 = document.querySelector(".main-text h2");
+ var mainTextMainBtn = document.querySelector(".main-text a");
+  if(!preloader.classList.contains("done")) {
+    setTimeout(function () { 
+      preloader.classList.add("done");
+      mainTextH1.classList.add("animate-h1");
+      mainTextH2.classList.add("animate-h2");
+      mainTextMainBtn.classList.add("animate-btn");
+    },2000)
+  }
 
-    // var menu = document.querySelector(".menu");
-    // menu.addEventListener('click', function () { 
-    //     menu.classList.toggle("active");
-    // });
-
-     $(".menu").on("click", function () { 
-         $(this).toggleClass("active");
-         $(".overlay").toggleClass("menu-open");
-      });
-     $(".nav a").on("click", function () { 
-        $(".menu").toggleClass("active");
-        $(".overlay").toggleClass("menu-open");
-      });
-    // menu toggle END
 });
